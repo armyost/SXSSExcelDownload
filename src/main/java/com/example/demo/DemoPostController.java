2467c52613f18c2308ee6469cab1d94421ab06cd
김종포 <armyost@naver.com>
2022-05-30 06:18:27 +0900

    Apply Swagger Document plugin

--- src/main/java/com/example/demo/DemoPostController.java
+++ src/main/java/com/example/demo/DemoPostController.java
@@ -5,15 +5,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.annotations.ApiOperation;
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 
 @RestController
 // value : localhost:8080/posts로 접근할 경우 이 controller에 접근한다.
-// produces : 생산 가능한 미디어 타입을 지정해서 일치할 때만 요청을 매칭함. (매핑 제한용)
-// comsumes : 소비 가능한 미디어 타입을 지정해서 일치할 때만 요청을 매칭함. (매핑 제한용)
+// produces : 반환타입을 강제
+// comsumes : 소비 가능한 미디어 타입을 지정해서 일치할 때만 요청을 매칭
 @RequestMapping(value = "/posts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DemoPostController {
 
@@ -23,6 +24,7 @@ public class DemoPostController {
     //게시글 추가(HTTP 메서드 : POST, 본문 데이터형식 : JSON, 경우 메시지 소비)
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
     //ResponseEntity 상태코드 제어
+    @ApiOperation(value = "Swagger Sample - GET", notes = "게시글 추가(HTTP 메서드 : POST, 본문 데이터형식 : JSON, 경우 메시지 소비)")
     public ResponseEntity<Void> createPost(@RequestBody Map<String, String> requestBody){
   
 	    System.out.println("createPost");
@@ -39,6 +41,7 @@ public class DemoPostController {
     //ex) localhost:8080/posts/1 
     @GetMapping(value = "/{postId}")
     //PathVariable은 URI에 넘어온 postId 값을 가져오기 위해 사용
+    @ApiOperation(value = "Swagger Sample - POST", notes = "특정 게시글 조회(HTTP 메서드 : GET, 본문 데이터형식 : 없음, 경우 메시지 소비)")
     public DemoEntity getPost(@PathVariable String postId){
 
         System.out.println("ListPost");
@@ -50,6 +53,7 @@ public class DemoPostController {
     
     //특정 게시글 업데이트(HTTP 메서드 : PUT, 본문 데이터형식 : 없음, 경우 메시지 소비)
     @PutMapping(value = "/{postId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Swagger Sample - PUT", notes = "특정 게시글 업데이트(HTTP 메서드 : PUT, 본문 데이터형식 : 없음, 경우 메시지 소비)")
     public ResponseEntity<Void> updatePost(@PathVariable String postId, @RequestBody Map<String, String> requestBody){
 
         System.out.println("UpdatePost");
@@ -63,7 +67,8 @@ public class DemoPostController {
 
     //특정 게시글 삭제(HTTP 메서드 : DELETE, 본문 데이터형식 : 없음, 경우 메시지 소비)
     @DeleteMapping("/{postId}")
-        public ResponseEntity<Void> deletePost(@PathVariable String postId){
+    @ApiOperation(value = "Swagger Sample - DELETE", notes = "특정 게시글 삭제(HTTP 메서드 : DELETE, 본문 데이터형식 : 없음, 경우 메시지 소비)")
+    public ResponseEntity<Void> deletePost(@PathVariable String postId){
 
         System.out.println("DeletePost");
         //Business Logic 추가

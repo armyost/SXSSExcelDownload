afc236aba3c3b18e5e589ef74dfb2c6db2f4091c
김종포 <armyost@naver.com>
2023-04-26 13:11:11 +0900

    230426v1

--- src/main/java/com/example/demo/DemoPostController.java
+++ src/main/java/com/example/demo/DemoPostController.java
@@ -9,72 +9,87 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.ApiOperation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.sql.Timestamp;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
 
 @RestController
-// value : localhost:8080/posts로 접근할 경우 이 controller에 접근한다.
-// produces : 반환타입을 강제
-// comsumes : 소비 가능한 미디어 타입을 지정해서 일치할 때만 요청을 매칭
-@RequestMapping(value = "/posts", produces = MediaType.APPLICATION_JSON_VALUE)
+
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DemoPostController {
 
-    //샘플을 위하여 서블릿 내부에서 데이터 저장토록 구성. 해당 샘플을 재사용할경우 DB에 데이터 저장을 권장
-    ArrayList<DemoEntity> demoEntities= new ArrayList<>();
-
-    //게시글 추가(HTTP 메서드 : POST, 본문 데이터형식 : JSON, 경우 메시지 소비)
-    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
-    //ResponseEntity 상태코드 제어
-    @ApiOperation(value = "Swagger Sample - POST", notes = "게시글 추가(HTTP 메서드 : POST, 본문 데이터형식 : JSON, 경우 메시지 소비)")
-    public ResponseEntity<String> createPost(@RequestBody Map<String, String> requestBody){
-  
-	    System.out.println("createPost");
+    @GetMapping(value = "/ping")
+    public String getPing(){
+        String response_val = "pong";
+        System.out.println("Ping has been occurred");
         //Business Logic 추가
-        DemoEntity demoEntity = new DemoEntity();
-        demoEntity.setPostId(requestBody.get("postId"));
-        demoEntity.setTitle(requestBody.get("title"));
-        demoEntity.setContents(requestBody.get("contents"));
-        demoEntities.add(demoEntity);
-        // return new ResponseEntity<Void>(HttpStatus.OK);
-        return new ResponseEntity<>("result successful", HttpStatus.OK);
-    }
-    
-    //특정 게시글 조회(HTTP 메서드 : GET, 본문 데이터형식 : 없음, 경우 메시지 소비)
-    //ex) localhost:8080/posts/1 
-    @GetMapping(value = "/{postId}")
-    //PathVariable은 URI에 넘어온 postId 값을 가져오기 위해 사용
-    @ApiOperation(value = "Swagger Sample - GET", notes = "특정 게시글 조회(HTTP 메서드 : GET, 본문 데이터형식 : 없음, 경우 메시지 소비)")
-    public DemoEntity getPost(@PathVariable String postId){
-
-        System.out.println("ListPost");
-        //Business Logic 추가
-        DemoEntity demoEntity = new DemoEntity();
-        demoEntity=demoEntities.get(Integer.parseInt(postId)-1);
-        return demoEntity;
-    }
-    
-    //특정 게시글 업데이트(HTTP 메서드 : PUT, 본문 데이터형식 : 없음, 경우 메시지 소비)
-    @PutMapping(value = "/{postId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Swagger Sample - PUT", notes = "특정 게시글 업데이트(HTTP 메서드 : PUT, 본문 데이터형식 : 없음, 경우 메시지 소비)")
-    public ResponseEntity<String> updatePost(@PathVariable String postId, @RequestBody Map<String, String> requestBody){
-
-        System.out.println("UpdatePost");
-        //Business Logic 추가
-        DemoEntity demoEntity = demoEntities.get(Integer.parseInt(postId)-1);
-        demoEntity.setPostId(requestBody.get("postId"));
-        demoEntity.setTitle(requestBody.get("title"));
-        demoEntity.setContents(requestBody.get("contents"));
-        return new ResponseEntity<>("result successful", HttpStatus.OK);
+        return response_val;
     }
 
-    //특정 게시글 삭제(HTTP 메서드 : DELETE, 본문 데이터형식 : 없음, 경우 메시지 소비)
-    @DeleteMapping("/{postId}")
-    @ApiOperation(value = "Swagger Sample - DELETE", notes = "특정 게시글 삭제(HTTP 메서드 : DELETE, 본문 데이터형식 : 없음, 경우 메시지 소비)")
-    public ResponseEntity<String> deletePost(@PathVariable String postId){
+    @GetMapping(value = "/excelDownload")
+    public void excelDownload(HttpServletResponse response) throws IOException{
 
-        System.out.println("DeletePost");
-        //Business Logic 추가
-        demoEntities.remove(Integer.parseInt(postId)-1);
-        return new ResponseEntity<>("result successful", HttpStatus.OK);
+        List<Map<String, Object>> datas = new ArrayList<>();
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println(timestamp+" Excel Download Called by User");
+
+        int i =0;
+        int testSize = 100000;
+
+        while(i < testSize){
+            Map<String, Object> data1 = new HashMap<>();
+            String test_value_a = String.format("%d_test_value_a",i);
+            String test_value_b = String.format("%d_test_value_b",i);
+            String test_value_c = String.format("%d_test_value_c",i);
+            String test_value_d = String.format("%d_test_value_d",i);
+            String test_value_e = String.format("%d_test_value_e",i);
+            String test_value_f = String.format("%d_test_value_f",i);
+            String test_value_g = String.format("%d_test_value_g",i);
+            String test_value_h = String.format("%d_test_value_h",i);
+            String test_value_i = String.format("%d_test_value_i",i);
+            String test_value_j = String.format("%d_test_value_j",i);
+            String test_value_k = String.format("%d_test_value_k",i);
+            String test_value_l = String.format("%d_test_value_l",i);
+            data1.put("a", test_value_a);
+            data1.put("b", test_value_b);
+            data1.put("c", test_value_c);
+            data1.put("d", test_value_d);
+            data1.put("e", test_value_e);
+            data1.put("f", test_value_f);
+            data1.put("g", test_value_g);
+            data1.put("h", test_value_h);
+            data1.put("i", test_value_i);
+            data1.put("j", test_value_j);
+            data1.put("k", test_value_k);
+            data1.put("l", test_value_l);
+
+            datas.add(data1);
+            
+            i++;
+        }
+
+        ExcelUtil excelUtil = new ExcelUtil();
+        excelUtil.createExcelToResponse(
+            datas,
+            String.format("%s-%s", "data", LocalDate.now().toString()),
+            response
+        );
     }
-}
+
+} 
